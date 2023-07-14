59ef51c3909a44cd8e809ca7d447af93b7677014
Success Hycenth <successhycenth8@gmail.com>
2023-07-14 02:12:47 +0100

    moved meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
 import Hero from "./components/Hero";
 
+export const metadata: Metadata = {
+  title: "Naeme Events - BOOK YOUR TICKETS SEAMLESSLY",
+  description: "Discover amazing events happening around you.",
+};
 export default async function Home() {
   return (
     <main className="">
