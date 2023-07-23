3b5f7d5b2eef1768aa8813b9ee16daa9a211a6b2
Success Hycenth <successhycenth8@gmail.com>
2023-07-23 12:15:05 +0100

    update

--- api.ts
+++ api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
-console.log({ NEXT_PUBLIC_SERVER_URL });
 const api = axios.create({
   baseURL: `${NEXT_PUBLIC_SERVER_URL}`,
 });
