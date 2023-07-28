090523ffc47bea910208db6c40b589ca3776ba65
Success Hycenth <successhycenth8@gmail.com>
2023-07-28 07:37:12 +0100

    update

--- src/app/events/components/Skeleton.tsx
+++ src/app/events/components/Skeleton.tsx
@@ -2,7 +2,7 @@ export function Skeleton() {
   return (
     <div
       data-aos="fade-up"
-      className="card card-compact w-full max-w-96 h-80 bg-gray-100 animate-pulse rounded-3xl"
+      className="card card-compact w-full max-w-96 h-80 bg-gray-100 animate-pulse rounded-3xl mx-6"
     >
       <figure className="h-40 w-full bg-gray-200 animate-pulse"></figure>
       <h2 className="card-title h-10 my-5 w-4/5 bg-gray-200 animate-pulse"></h2>
