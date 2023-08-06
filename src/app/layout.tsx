5621fefb7ba8c558037292a70eb6e4550b29772a
Success Hycenth <successhycenth8@gmail.com>
2023-08-06 10:19:44 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Montserrat } from "next/font/google";
 
 import { UserProvider } from "@/context/UserContext";
 import { CartProvider } from "@/context/CartContext";
+import { Head } from "next/document";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -16,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="tiYKhZrgpRhZsF1Ly0VMZ1JeTOxaSIl0pICVYQ0FfGI"
+        />
+      </head>
       <body className={montserrat.className}>
         <UserProvider>
           <CartProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
 import Hero from "./components/Hero";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Naeme Events - BOOK YOUR TICKETS SEAMLESSLY",
@@ -9,12 +8,6 @@ export const metadata: Metadata = {
 export default async function Home() {
   return (
     <main className="bg-gray-100">
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="tiYKhZrgpRhZsF1Ly0VMZ1JeTOxaSIl0pICVYQ0FfGI"
-        />
-      </Head>
       <Hero />
     </main>
   );
