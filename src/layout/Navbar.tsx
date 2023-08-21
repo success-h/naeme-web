787d518c6453f542e4db32ab63a8993bf1474b09
Success Hycenth <successhycenth8@gmail.com>
2023-08-21 23:51:40 +0100

    update

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -50,53 +50,52 @@ export function Navbar() {
                 priority
               />
             </Link>
-            <div className="">
-              <div className="flex-none gap-2 z-50">
-                <div className="dropdown dropdown-bottom dropdown-end">
-                  <label
-                    tabIndex={0}
-                    className="lg:hidden btn hover:border-none btn-ghost btn-circle hover:bg-white"
-                  >
-                    <div className="indicator">
-                      <Image
-                        priority
-                        height={20}
-                        width={20}
-                        src="/Buy.svg"
-                        alt="buy"
-                      />
+            <div className="flex gap-x-4">
+              <div className="">
+                <div className="flex-none gap-2 z-50">
+                  <div className="dropdown dropdown-bottom dropdown-end">
+                    <label
+                      tabIndex={0}
+                      className="lg:hidden btn hover:border-none btn-ghost btn-circle hover:bg-white"
+                    >
+                      <div className="indicator">
+                        <Image
+                          priority
+                          height={20}
+                          width={20}
+                          src="/Buy.svg"
+                          alt="buy"
+                        />
 
-                      <span className="badge badge-md indicator-item bg-blue-400 border-none text-lg">
-                        {cartQuantity}
-                      </span>
-                    </div>
-                  </label>
-                  <div
-                    tabIndex={0}
-                    className="mt-3 z-[1] bg-gray-100 card card-compact dropdown-content w-52 shadow"
-                  >
-                    <div className="card-body">
-                      <span className="font-bold text-lg">
-                        {cartQuantity} Items
-                      </span>
-                      <span className="text-info">
-                        Subtotal: ${cartTotal()}
-                      </span>
+                        <span className="badge badge-md indicator-item bg-blue-400 border-none text-lg">
+                          {cartQuantity}
+                        </span>
+                      </div>
+                    </label>
+                    <div
+                      tabIndex={0}
+                      className="mt-3 z-[1] bg-gray-100 card card-compact dropdown-content w-52 shadow"
+                    >
+                      <div className="card-body">
+                        <span className="font-bold text-lg">
+                          {cartQuantity} Items
+                        </span>
+                        <span className="text-info">
+                          Subtotal: ${cartTotal()}
+                        </span>
 
-                      <Link href="/cart" className="">
-                        <button className="btn bg-blue-300 w-full">
-                          View cart
-                        </button>
-                      </Link>
+                        <Link href="/cart" className="">
+                          <button className="btn bg-blue-300 w-full">
+                            View cart
+                          </button>
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="flex-none lg:hidden">
-                <label
-                  htmlFor="my-drawer-3"
-                  className="btn btn-square btn-ghost"
-                >
+              <div className="dropdown dropdown-left lg:hidden">
+                <label tabIndex={0} className="btn btn-ghost btn-circle">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -111,6 +110,75 @@ export function Navbar() {
                     ></path>
                   </svg>
                 </label>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content mt-16 -mr-10 z-[1] p-2 shadow bg-zinc-900 rounded-box"
+                >
+                  <div className="bg-zinc-900 z-30 menu p-4 w-64 h-full gap-y-4 text-gray-500">
+                    <Link href="/" className="px-2 cursor-pointer my-5">
+                      <Image
+                        className="sm:h-[17px] justify-center self-center"
+                        src="/logo.svg"
+                        height={20}
+                        width={120}
+                        priority
+                        alt="logo"
+                      />
+                    </Link>
+                    <li>
+                      <Link href="/events">Explore events</Link>
+                    </li>
+                    <li>
+                      <Link href="/scan-ticket">Scan Ticket</Link>
+                    </li>
+                    <li>
+                      <Link href={user ? "/create-event" : "/signin"}>
+                        Create events
+                      </Link>
+                    </li>
+
+                    <li>
+                      {user ? (
+                        <div className="dropdown dropdown-bottom text-gray-600">
+                          <label
+                            tabIndex={0}
+                            className="btn btn-ghost btn-circle avatar"
+                          >
+                            <div className="w-10 rounded-full">
+                              <Image
+                                width={40}
+                                alt="image"
+                                height={40}
+                                src={user.image}
+                              />
+                            </div>
+                          </label>
+                          <div
+                            tabIndex={0}
+                            className="bg-zinc-800 menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52"
+                          >
+                            <p className="mb-3">
+                              <Link
+                                href="/dashboard"
+                                className="justify-between"
+                              >
+                                Profile
+                              </Link>
+                            </p>
+
+                            <p>
+                              <button onClick={logout}>Logout</button>
+                            </p>
+                          </div>
+                        </div>
+                      ) : (
+                        <p>
+                          <Link href="/signin">Sign In</Link>
+                        </p>
+                      )}
+                    </li>
+                  </div>
+                </ul>
               </div>
             </div>
             <div className="flex-none hidden lg:block">
@@ -203,70 +271,6 @@ export function Navbar() {
               </ul>
             </div>
           </div>
-        </div>
-        <div className="drawer-side">
-          <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-          <ul className="bg-zinc-900 z-30 menu p-4 w-64 h-full gap-y-4 text-gray-500">
-            <Link href="/" className="px-2 cursor-pointer my-5">
-              <Image
-                className="sm:h-[17px] justify-center self-center"
-                src="/logo.svg"
-                height={20}
-                width={120}
-                priority
-                alt="logo"
-              />
-            </Link>
-            <li>
-              <Link href="/events">Explore events</Link>
-            </li>
-            <li>
-              <Link href="/scan-ticket">Scan Ticket</Link>
-            </li>
-            <li>
-              <Link href={user ? "/create-event" : "/signin"}>
-                Create events
-              </Link>
-            </li>
-
-            <li>
-              {user ? (
-                <div className="dropdown dropdown-bottom text-gray-600">
-                  <label
-                    tabIndex={0}
-                    className="btn btn-ghost btn-circle avatar"
-                  >
-                    <div className="w-10 rounded-full">
-                      <Image
-                        width={40}
-                        alt="image"
-                        height={40}
-                        src={user.image}
-                      />
-                    </div>
-                  </label>
-                  <div
-                    tabIndex={0}
-                    className="bg-zinc-800 menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52"
-                  >
-                    <p className="mb-3">
-                      <Link href="/dashboard" className="justify-between">
-                        Profile
-                      </Link>
-                    </p>
-
-                    <p>
-                      <button onClick={logout}>Logout</button>
-                    </p>
-                  </div>
-                </div>
-              ) : (
-                <p>
-                  <Link href="/signin">Sign In</Link>
-                </p>
-              )}
-            </li>
-          </ul>
         </div>
       </div>
     </nav>
